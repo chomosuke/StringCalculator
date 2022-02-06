ae8e6c5244fa2094e56d9b28b55930241f1ce605
Richard Li <richard.li@detectorinspector.com.au>
2022-02-07 10:01:35 +1100

    test have any delimiters passed

--- src/add.ts
+++ src/add.ts
@@ -1,7 +1,14 @@
-
 export function add(str: string): number {
     if (str == '') {
         return 0;
     }
-    return str.split(/[,\n]/i).map(n => parseInt(n)).reduce((p, c) => p + c);
+
+    let delimiter: RegExp | string = /[,\n]/i;
+    
+    if (str.length >= 2 && str.substring(0, 2) == '//') {
+        const firstBreak = str.indexOf('\n');
+        delimiter = str.substring(2, firstBreak);
+        str = str.substring(firstBreak + 1);
+    }
+    return str.split(delimiter).map(n => parseInt(n)).reduce((p, c) => p + c);
 }
