3c4923f8bdd5b47111f805840cc3cc4920409d1a
Richard Li <richard.li@detectorinspector.com.au>
2022-02-07 09:37:38 +1100

    second test written

--- src/__tests__/tests.ts
+++ src/__tests__/tests.ts
@@ -3,5 +3,10 @@ import { add } from "../main";
 describe('add tests', () => {
     test('empty string returns 0', () => {
         expect(add('')).toStrictEqual(0);
-    })
+    });
+
+    test('num string return the number', () => {
+        expect(add('1')).toStrictEqual(1);
+        expect(add('3')).toStrictEqual(3);
+    });
 });
